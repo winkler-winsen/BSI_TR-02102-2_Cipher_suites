07ef170f84a9cc1db390f8fcf5bc7cb3f8ecca11
Lars Winkler <43114182+winkler-winsen@users.noreply.github.com>
2023-03-28 15:59:42 +0200

    add PS Script

---
+++ SetTlsBSI.ps1
@@ -0,0 +1,8 @@
+﻿$TLS12CS = Get-Content 'BSI TR-02102-2 TLS1.2 PFS Cipher Suites.txt'
+$TLS13CS = Get-Content 'BSI TR-02102-2 TLS1.3 Cipher Suites.txt'
+
+Foreach ($CS in (Get-TlsCipherSuite -Name ''))
+{Disable-TlsCipherSuite -Name $CS.Name}
+
+Foreach ($CS in $TLS12CS)
+{Enable-TlsCipherSuite -Name $CS}
