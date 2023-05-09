b99e5dcf7cb2729b21f51668bcf3507948daae80
Lars Winkler <43114182+winkler-winsen@users.noreply.github.com>
2023-05-09 11:03:15 +0200

    Update SetTlsBSI.ps1 add backup

--- SetTlsBSI.ps1
+++ SetTlsBSI.ps1
@@ -5,6 +5,9 @@
 $TLS12CS = Get-Content 'BSI TR-02102-2 TLS1.2 PFS Cipher Suites.txt'
 $TLS13CS = Get-Content 'BSI TR-02102-2 TLS1.3 Cipher Suites.txt'
 
+# backup active TlsCipherSuites to file
+(Get-TlsCipherSuite).Name |Out-File -FilePath "TlsCipherSuitsBackup $(Get-Date -Format 'yyyyMMddHHMMss').txt"
+
 # remove all TlsCipherSuites
 Foreach ($CS in (Get-TlsCipherSuite -Name ''))
 {Disable-TlsCipherSuite -Name $CS.Name}
